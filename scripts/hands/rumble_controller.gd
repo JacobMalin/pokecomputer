3b0b0813ac072ed8543c45ccd24099b94eff8d64
Jacob <malin146@umn.edu>
2023-12-14 04:07:26 -0600

    fuck portals

--- scripts/hands/rumble_controller.gd
+++ scripts/hands/rumble_controller.gd
@@ -1,17 +1,21 @@
 class_name RumbleController
 extends XRController3D
 
+@export var do_rumble = true
+
+### Helper ###
+
 func rumble():
-	trigger_haptic_pulse("haptic", 0, 0.1, 0.1, 0)
+	if do_rumble: trigger_haptic_pulse("haptic", 0, 0.1, 0.1, 0)
 
 func enter_rumble():
-	trigger_haptic_pulse("haptic", 0, 0.15, 0.1, 0)
+	if do_rumble: trigger_haptic_pulse("haptic", 0, 0.15, 0.1, 0)
 
 func exit_rumble():
-	trigger_haptic_pulse("haptic", 0, 0.01, 0.05, 0)
+	if do_rumble: trigger_haptic_pulse("haptic", 0, 0.01, 0.05, 0)
 
 func full_rumble():
-	trigger_haptic_pulse("haptic", 0, 0.1, 0.15, 0)
+	if do_rumble: trigger_haptic_pulse("haptic", 0, 0.1, 0.15, 0)
 
 func empty_rumble():
-	trigger_haptic_pulse("haptic", 0, 0.15, 0.07, 0)
+	if do_rumble: trigger_haptic_pulse("haptic", 0, 0.15, 0.07, 0)
