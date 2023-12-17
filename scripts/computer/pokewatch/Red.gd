7864e709d0073a8b17d4d2114c51e85485b978b6
lars6866 <lars6866@umn.edu>
2023-12-16 18:12:34 -0600

    added working collision shapes to pokewatch

---
+++ scripts/computer/pokewatch/Red.gd
@@ -0,0 +1,19 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func _on_finger_entered(area):
+	if area.is_in_group("index"):
+		# anim.play("press")
+		print("red")
+
+		# Rumble
+		area.rumble()
